15aa27a4c710dbe358d9dbd2618beddeaaa3bf53
limseungkyun <qwecc2@gmail.com>
2021-06-15 11:34:23 +0900

    Initial Commit

---
+++ aicatstate_watch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  aicatstate_watch WatchKit Extension
+//
+//  Created by iDo on 2021/06/15.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
